8627b96a8ec1eff8476f653f844939fbd2d183ec
MOHAMEDRIASKHAN <mohamedriaskhan@gmail.com>
2022-09-05 01:48:56 -0700

    MoodAnalyser3

---
+++ MoodAnayl.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnaylserUC3
+{
+    public class MoodAnayl
+    {
+        private const string message = "I am in Sad Mood";
+        private string testmessage = "";
+        
+        public MoodAnayl()
+        {
+
+        }
+        public MoodAnayl(string msg)
+        {
+            this.testmessage = msg;
+        }
+
+        public string AnalyseMood()
+        {
+
+            try
+
+            {
+                if (this.testmessage == "NULL" || this.testmessage == string.Empty)
+                {
+                    throw new Exception("Mood is Null");
+                }
+
+                if (this.testmessage == message)
+                {
+                    return "SAD";
+                }
+                else if (this.testmessage == "I am in Happy Mood")
+                {
+                    return "Happy";
+                }
+                else
+                {
+                    throw new Exception("Mood is Null");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return "Happy";
+            }
+
+
+        }
+    }
+}
